5aea827b2aa160a1ca626adcde7c0f05694b7066
A_Botir <shirokirutsuga>
2024-03-03 23:44:37 +0500

    firdt commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div></div>
+    </>
+  )
+}
+
+// api_key="67b950c76555ffb6628f502a1eb9921a"
+
+export default App
